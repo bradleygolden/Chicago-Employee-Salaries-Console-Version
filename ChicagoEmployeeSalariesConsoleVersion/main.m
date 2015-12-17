a2b1644213d58e4dff9590e1352ecfbf04364390
Bradley Golden <golden.bradley@gmail.com>
2015-12-17 11:23:07 -0800

    Initial Commit

---
+++ ChicagoEmployeeSalariesConsoleVersion/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ChicagoEmployeeSalariesConsoleVersion
+//
+//  Created by Bradley Golden on 12/17/15.
+//  Copyright © 2015 Bradley Golden. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
